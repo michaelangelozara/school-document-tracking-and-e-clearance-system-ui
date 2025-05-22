26bc5b9a3cd27ad1e25c212ba9c13c224ad0e75e
Michael Angelo B. Zara <michaelangelobuccatzara@gmail.com>
2025-05-22 20:58:20 +0800

    Signature Card modal's api integration implementated

--- src/components/signature/SignatureCard.tsx
+++ src/components/signature/SignatureCard.tsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 
 import { getMySignature } from "../../service/UserService";
 import { useAuth } from "../../context/AuthContext";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store/Store";
+import { save, updateLoadingStatus } from "../../store/slice/ESignatureSlice";
+import { getErrorMessage } from "../../helper/AxiosHelper";
+import { open } from "../../store/slice/MessageSlice";
 
 const SignatureCard = () => {
   const [isESignatureLoading, setIsESignatureLoading] =
@@ -9,15 +14,20 @@ const SignatureCard = () => {
 
   const [eSignature, setESignature] = useState<string | null>(null);
   const { apiClient } = useAuth();
+  const dispatch = useDispatch<AppDispatch>();
   const getESignature = async () => {
     try {
       setIsESignatureLoading(true);
+      dispatch(updateLoadingStatus(true));
       const response = await getMySignature(apiClient);
       setESignature(response);
-    } catch (error) {
-      console.log(error);
+      dispatch(save(response));
+    } catch (err) {
+      const errorMessage = getErrorMessage(err);
+      dispatch(open(errorMessage));
     } finally {
       setIsESignatureLoading(false);
+      dispatch(updateLoadingStatus(false));
     }
   };
 
